5841277f31609a23aa08020942cd8d8a6f653147
João Miranda <joao.miranda@carrotincentives.com>
2024-07-11 01:47:08 +0100

    added populate db  script, which is fully working

--- Database/init-db/041_tournament_match.sql
+++ Database/init-db/041_tournament_match.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS transcendence.tournament_match
 (
 	id SERIAL PRIMARY KEY
 	, tournament_id INT NOT NULL
-	, round INT NOT NULL
 	, match_id INT NOT NULL
+	, round INT NOT NULL
 );
 
 ALTER TABLE IF EXISTS transcendence.tournament_match OWNER to "admin";
