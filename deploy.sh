b9ac0ed7ef70bd36cc569f6df610da44e6bfc783
ahokcool <stein.alexander@posteo.de>
2024-08-27 15:05:54 +0200

    added the parse args function

--- deploy.sh
+++ deploy.sh
@@ -56,7 +56,13 @@ RD='\033[0;31m'
 YL='\033[0;33m'
 OR='\033[38;5;208m'
 GR='\033[0;32m'
+BL='\033[0;36m'
 NC='\033[0m'
+
+# Global Variables for the script
+NEW_ENV_PATH=""
+COMMAND=""
+CONTAINERS=""
 
 FE_CONTAINER_NAME="fe"
 BE_CONTAINER_NAME="be"
@@ -79,11 +85,11 @@ REMAINING_ARGS=()
 # Global variable to track the spinner PID
 SPINNER_PID=""
 
-# FUNCTIONS
+# PRINT FUNCTIONS
 # ------------------------------------------------------------------------------ 
 # Function to print a important message in color
 print_header() {
-    printf "$1 >>> %s${NC}\n" "$2"
+	printf "$1 >>> %b${NC}\n" "$2"
 }
 
 # Function to print error messages in red and exit the script
@@ -91,17 +97,25 @@ print_error() {
 	print_header "${RD}" "Error: $1"
     exit 1
 }
-
-print_success() {
-	echo -en "\u2705 "  # Green checkmark emoji
-	echo $1
-}
-
-print_fail() {
-	print_header "${RD}" "Failed: $1"
-}
-
-# Function to start the spinner with a message
+# ------------------------------------------------------------------------------ 
+# SPINNER FUNCTIONS
+# ------------------------------------------------------------------------------ 
+# To make thinks pretty we use a spinner to show that the script is working.
+# The spinner allows ous to nest a task in it and show a message while the task 
+# is running. The result of the task will be shown after the spinner stops.
+# USAGE:
+# 	perform_task_with_spinner "Message" "Task" "Failure Message" "Fail Continue"
+#		- Message: The message to show while the task is running
+#		- Task: The task to perform
+#		- Failure Message: The message to show if the task fails
+#		- Fail Continue: If the task fails, should the script continue or exit
+#
+#	Example:
+#		perform_task_with_spinner "Checking if the file exists" '[ -f "$FILE" ]' "File does not exist" false
+# 
+# The functions start_spinner & stop_spinner are helper functions for the
+# spinner and should not be called directly!
+# ------------------------------------------------------------------------------ 
 start_spinner() {
     local message="$1"
     local spin='-\|/'
@@ -118,7 +132,7 @@ start_spinner() {
     SPINNER_PID=$!
 }
 
-# Function to stop the spinner and update the message
+# see comment block above
 stop_spinner() {
     local exit_code=$1
     local message="$2"
@@ -145,7 +159,7 @@ stop_spinner() {
 	return $exit_code
 }
 
-# Function to perform a task with a spinner
+# see comment block above
 perform_task_with_spinner() {
     local message="$1..."
     local task="$2"
@@ -163,10 +177,103 @@ perform_task_with_spinner() {
     stop_spinner "$exit_code" "$message" "$failure_message" "$fail_continue"
 	return $?
 }
+# ------------------------------------------------------------------------------ 
+# SETUP FUNCTIONS
+# ------------------------------------------------------------------------------
+# As a short pre-setup the arguments are processed via parse_args. This function
+# is always called by the main script to assign the global variables:
+# - NEW_ENV_PATH
+# - COMMAND
+# - CONTAINERS
+# 
+# The main function check_setup is called by the main script to check if the
+# environment is set up correctly. It checks:
+# 	- if the .env file is linked and loaded
+#	- if the folders for the volumes are there and have the right permissions
+# Those checks are bundelt into one function so that the main script can call it
+# with one command and only if needed. (The cmds help and fclean dont need it)
+#
+# Besides the function check_setup all other functions are helper functions and
+# should not be called directly!
+# ------------------------------------------------------------------------------ 
+parse_args()
+{
+	print_header "${BL}" "Parsing arguments..."
+	NEW_ENV_PATH=""
+	COMMAND=""
+	CONTAINERS=""
+	ENV_FLAG_FOUND=false
+	MSG_MISSING_PATH="The -e flag is used but the path is missing. Flag will be ignored!"
+	# Main logic for Docker commands
+	# Loop through all the arguments
+	for arg in "$@"
+	do
+		if [ "$arg" == "-e" ]; then
+			NEW_ENV_PATH=$MSG_MISSING_PATH	# So the call ./deploy.sh start -e will fail (the path is missing)
+			ENV_FLAG_FOUND=true
+		elif [ "$ENV_FLAG_FOUND" == true ]; then
+			NEW_ENV_PATH=$arg
+			ENV_FLAG_FOUND=false
+		elif [ "$COMMAND" == "" ]; then
+			COMMAND=$arg
+		else
+			CONTAINERS="$CONTAINERS $arg"
+		fi
+	done
 
-# Function to check if an .env file is linked
+	# Fallbacks for the variable
+	# 	Default command is start
+	COMMAND="${COMMAND:-start}"			
+	echo -e "COMMAND:\t$COMMAND"
+	CONTAINERS="${CONTAINERS:-$ALL_SERVICES}"	# If no container is specified, all containers will be affected
+
+	# 	Containers can't be specified for the commands fclean, re and help
+	if [[ "$COMMAND" == "fclean" || "$COMMAND" == "re" || "$COMMAND" == "help" ]] && [[ "$CONTAINERS" != "$ALL_SERVICES" ]]; then
+		echo -e $OR "Containers can't be specified for the commands fclean, re and help" $NC
+		echo -e $OR "The input '$CONTAINERS' will be ignored" $NC
+		CONTAINERS=""
+	fi
+	# 	Containers can only be those from the $ALL_SERVICES list
+	for item in $CONTAINERS; do
+        if ! [[ " $ALL_SERVICES " =~ " $item " ]]; then
+            print_error "Invalid container: '$item' (only '$ALL_SERVICES' are allowed!)"
+        fi
+    done
+	echo -e "CONTAINERS:\t$CONTAINERS"
+
+	#	Environment path is not set
+	if [ "$NEW_ENV_PATH" == "$MSG_MISSING_PATH" ]; then
+		echo -e $OR $MSG_MISSING_PATH $NC
+		NEW_ENV_PATH=""
+	fi
+	echo -e "NEW_ENV_PATH:\t$NEW_ENV_PATH"
+	print_header "${BL}" "Parsing arguments...${GR}DONE${NC}"
+	exit 1
+}
+
+check_setup() {
+	# CHECK FOR ENV FILE and process arguments
+check_env_link "$@"
+#TODO: PROBLEM THAT THE -e cant be removed if this function is called after the switch in main
+# UPDATE ARGUMENTS: Use the global REMAINING_ARGS variable
+# Set positional parameters for further use
+# this needs to be done since the check_env_link function may use the -e flag
+set -- "${REMAINING_ARGS[@]}"
+
+# CHECK IF FOLDERS ARE THERE for the volumes
+check_volume_folders
+
+
+
+	check_env_link "$@"
+	check_volume_folders
+}
+
+
+# Function to check (and update) the link to the .env file which will then be 
+# sourced and sample tested with $DB_NAME
 check_env_link() {
-	print_header "${YL}" "Checking for the .env file link..."
+	print_header "${BL}" "Checking for the .env file link..."
 	# Initialize an empty array to hold the remaining arguments
     local args=("$@")
 
@@ -218,18 +325,62 @@ check_env_link() {
 	
     # Assign remaining arguments to the global variable
     REMAINING_ARGS=("${args[@]}")
-	print_header "${GR}" "Checking for the .env file link...DONE"
+	print_header "${BL}" "Checking for the .env file link...${GR}DONE${NC}" 
 }
 
+# Used to prepare the folders for the docker volumes
+check_path_and_permission()
+{
+	local path=$1
+
+	if ! perform_task_with_spinner \
+		"Checking presents of folder: <$path>" \
+		'[ -e "$path" ]' \
+		"doesn't exist" \
+		true; then
+
+		# Create the folder
+		perform_task_with_spinner \
+			"Creating folder: <$path>" \
+			'mkdir -p $path' \
+			"couldn't create folder: <$path>!" \
+			false
+	fi
+	
+	if ! perform_task_with_spinner \
+		"Checking permissions of folder: <$path>" \
+		'[ -r "$path" ] && [ -w "$path" ]' \
+		"no read and write permissions!" \
+		true; then
+
+		# Change the ownership of the folder
+		perform_task_with_spinner \
+			"Changing ownership of folder: <$path>" \
+			'sudo chown -R "$USER:$USER" "$path"' \
+			"couldn't change ownership of folder: <$path>!" \
+			false
+	fi
+}
+
+# Function to check if the folders for the volumes are there
+check_volume_folders()
+{
+	print_header "${YL}" "Checking paths for volumes..."
+	check_path_and_permission $DB_VOLUME_PATH
+	check_path_and_permission $PA_VOLUME_PATH
+	print_header "${GR}" "Checking paths for volumes...${GR}DONE${NC}"
+}
+
+# ------------------------------------------------------------------------------ 
 # DOCKER BASIC FUNCTIONS
-#	start [contaier]		| down build up [THIS IS THE DEFAULT TARGET]
 #	stop [contaier]			| Stops the container(s)
+#	build [contaier]		| Building the container(s)
+#	start [contaier]		| down build up [THIS IS THE DEFAULT TARGET]
 #	clean [contaier]		| down + Deletes the container(s) and the image(s)
 #	fclean 					| Deletes the container(s) + Volumes + Network
-#	build [contaier]		| Building the container(s)
 #	reset [container]		| clean + start
 #	re 						| fclean + start
-
+# ------------------------------------------------------------------------------ 
 docker_stop() {
 	print_header "${YL}" "Stopping containers: $1"
 	docker-compose --env-file "$STORED_ENV_PATH" stop $1
@@ -274,7 +425,8 @@ docker_fclean() {
 	sudo rm -rf ${VOLUME_ROOT_PATH}
 	print_header "${OR}" "Deltete the link to the environment file..."
 	rm -f ".transcendence_env_path"
-	print_success "All containers, images, volumes, and network have been deleted."
+	# print_success "All containers, images, volumes, and network have been deleted."
+	# TODO CHANGE THIS
 }
 
 docker_reset() {
@@ -295,54 +447,57 @@ docker_re() {
 #       Main Script
 ################################################################################
 
-# CHECK FOR ENV FILE and process arguments
-check_env_link "$@"
+# Welcome Message
+echo ""
+perform_task_with_spinner \
+	"Welcome to Barely Alive" \
+	'sleep 1' \
+	"Failed to print welcome message." \
+	false
 
-# UPDATE ARGUMENTS: Use the global REMAINING_ARGS variable
-# Set positional parameters for further use
-set -- "${REMAINING_ARGS[@]}"
+# Assign the global variables from the arguments
+parse_args "$@"
 
-# CHECK IF FOLDERS ARE THERE AND FILES AND OTHER STUFF HERE
-# create folders for volumes if they don't exist yet:
-print_header ${GR} "Creating folder for volumes at: $DB_VOLUME_PATH"
-mkdir -p ${DB_VOLUME_PATH}
-#print_header ${OR} "Changing ownership of volume folder at: $DB_VOLUME_PATH"
-## TODO
-## THIS SHOULD NOT BE DONE IF FOLDER EXISTS
-#chown -R "$USER:$USER" "${DB_VOLUME_PATH}"
+# Show the command that will be executed
+echo ""
+perform_task_with_spinner \
+	"Starting to execute command: $COMMAND for container(s): $CONTAINER" \
+	'sleep 1' \
+	"Failed to show the command." \
+	false
 
-print_header ${GR} "Creating folders for volumes at: $PA_VOLUME_PATH"
-mkdir -p ${PA_VOLUME_PATH}
-#print_header ${OR} "Changing ownership of volume folder at: $PA_VOLUME_PATH"
-#chown -R "$USER:$USER" "${PA_VOLUME_PATH}"
 
-# Main logic for Docker commands
-COMMAND="${1:-start}"  				# Default to 'start' if no command is provided
-CONTAINER="${2:-$ALL_SERVICES}"	# Optional container name
-
+# Execute the command
 case "$COMMAND" in
 	help)
         cat ./deploy.sh | head -n ${HELP_ENDS_AT_LINE}
         ;;
     start)
+		check_setup #TODO: Check if this is needed
         docker_start "$CONTAINER"
         ;;
     stop)
+		check_setup #TODO: Check if this is needed
         docker_stop "$CONTAINER"
         ;;
     clean)
+		check_setup #TODO: Check if this is needed
 		docker_clean "$CONTAINER"
         ;;
     fclean)
+		check_setup #TODO: Check if this is needed
 		docker_fclean
         ;;
     build)
+		check_setup #TODO: Check if this is needed
         docker_build "$CONTAINER"
         ;;
 	reset)
+		check_setup #TODO: Check if this is needed
 		docker_reset "$CONTAINER"
         ;;
     re)
+		check_setup #TODO: Check if this is needed
 		docker_re
         ;;
     *)
