7ff6f0a56feb9cffa8b01096a2f2bd010b82f9eb
AshParker19 <ashparker4219@gmail.com>
2024-09-03 15:53:25 +0100

    fixed the things that were requested in the comments: removed secred key and other variables values from code, now it's in env. simplified api-path to api. refacrored handle exception funciton, now it's not nested anymore.

--- Backend/src/app/settings.py
+++ Backend/src/app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ry+-mwb0wgdn0koep$_!a0^k^59+z!pa6pmdi2aq9z143lxy4'
+SECRET_KEY = os.environ.get('BE_SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Backend/src/app/urls.py
+++ Backend/src/app/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    path('api-path/', include('rest_framework.urls')),
+    path('api/', include('rest_framework.urls')),
 ]
