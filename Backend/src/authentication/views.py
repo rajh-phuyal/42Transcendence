d0899e9ff789e09617398fbeb3bd2197b92be3e7
AshParker19 <ashparker4219@gmail.com>
2024-09-03 20:30:02 +0100

    fixed the issue with the secret key

--- Backend/src/authentication/views.py
+++ Backend/src/authentication/views.py
@@ -11,24 +11,23 @@ class RegisterView(generics.CreateAPIView):
     
     def handle_exception(self, exc):
         response = exception_handler(exc, self.get_exception_handler_context())
-        
-        if response is None:
-            return response
-        if isinstance(exc, serializers.ValidationError):
-            response.data = {'errors': response.data}
-            response.status_code = status.HTTP_400_BAD_REQUEST
-        elif isinstance(exc, exceptions.AuthenticationFailed):
-            response.data = {'detail': 'Authentication failed'}
-            response.status_code = status.HTTP_401_UNAUTHORIZED
-        elif isinstance(exc, exceptions.NotAuthenticated):
-            response.data = {'detail': 'Not authenticated'}
-            response.status_code = status.HTTP_401_UNAUTHORIZED
-        elif isinstance(exc, exceptions.PermissionDenied):
-            response.data = {'detail': 'Permission denied'}
-            response.status_code = status.HTTP_403_FORBIDDEN
-        elif isinstance(exc, exceptions.NotFound):
-            response.data = {'detail': 'Not found'}
-            response.status_code = status.HTTP_404_NOT_FOUND
-        else:
-            response.data = {'detail': str(exc)}
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        if response is not None:
+            if isinstance(exc, serializers.ValidationError):
+                response.data = {'errors': response.data}
+                response.status_code = status.HTTP_400_BAD_REQUEST
+            elif isinstance(exc, exceptions.AuthenticationFailed):
+                response.data = {'detail': 'Authentication failed'}
+                response.status_code = status.HTTP_401_UNAUTHORIZED
+            elif isinstance(exc, exceptions.NotAuthenticated):
+                response.data = {'detail': 'Not authenticated'}
+                response.status_code = status.HTTP_401_UNAUTHORIZED
+            elif isinstance(exc, exceptions.PermissionDenied):
+                response.data = {'detail': 'Permission denied'}
+                response.status_code = status.HTTP_403_FORBIDDEN
+            elif isinstance(exc, exceptions.NotFound):
+                response.data = {'detail': 'Not found'}
+                response.status_code = status.HTTP_404_NOT_FOUND
+            else:
+                response.data = {'detail': str(exc)}
+                response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return response
