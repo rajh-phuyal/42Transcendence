c96f39040fda0e30262cbe2d1c90af9b7b08da72
AshParker19 <ashparker4219@gmail.com>
2024-09-05 17:04:16 +0100

    initial commit for user app

--- Backend/src/entrypoint.sh
+++ Backend/src/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Apply database migrations
-python manage.py makemigrations
-python manage.py migrate
+echo 'Starting 'python manage.py makemigrations'' > migration_logs.txt
+echo '================' >> migration_logs.txt
+python manage.py makemigrations >> migration_logs.txt
+echo 'Starting 'python manage.py migrate'' >> migration_logs.txt
+python manage.py migrate >> migration_logs.txt
 
 # Start the Django server
 exec "$@"
