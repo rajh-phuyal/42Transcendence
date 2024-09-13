7192a0dcb8d814aaea90ccbce5dcb363dc20bda1
ahokcool <stein.alexander@posteo.de>
2024-09-13 21:30:30 +0200

    added a todo list of endpoints

--- Backend/src/user/urls.py
+++ Backend/src/user/urls.py
@@ -7,4 +7,15 @@ from .views import ProfileView
 
 urlpatterns = [
 	path('profile/<int:id>/', ProfileView.as_view(), name='profile'),
+
+	# TODO iplement those endpoints:
+	# Friend request system
+#    path('friend-request/send/', SendFriendRequestView.as_view(), name='send_friend_request'),
+#    path('friend-request/accept/', AcceptFriendRequestView.as_view(), name='accept_friend_request'),
+#    path('friend-request/decline/', DeclineFriendRequestView.as_view(), name='decline_friend_request'),
+#    path('friends/list/', ListFriendsView.as_view(), name='list_friends'),
+
+    # Blocking/unblocking users
+#    path('friends/block/', BlockUserView.as_view(), name='block_user'),
+#    path('friends/unblock/', UnblockUserView.as_view(), name='unblock_user'),
 ]
