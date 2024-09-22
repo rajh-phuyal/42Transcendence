bc5682628bb801a7c1d3108fde71f71bb8d50e77
AshParker19 <ashparker4219@gmail.com>
2024-09-22 19:23:55 +0100

    created a custom exception for Validations. now get_and_validate_data raises it if needed

--- Backend/src/user/utils.py
+++ Backend/src/user/utils.py
@@ -1,17 +1,18 @@
 from rest_framework.response import Response
 from rest_framework import status
+from .exceptions import ValidationException
 
 def get_and_validate_data(request, action, doer_name, target_name):
 
     doer = request.data.get(doer_name)
     if not doer:
-        return None, Response({'error': f'Key --> "{doer_name}".    {doer_name} must be provided'}, status=status.HTTP_400_BAD_REQUEST)
+        raise ValidationException(f'Key --> "{doer_name}".    {doer_name} must be provided')
     
     target = request.data.get(target_name)
     if not target:
-        return None, Response({'error': f'Key --> "{target_name}".    {target_name} must be provided'}, status=status.HTTP_400_BAD_REQUEST)
+        raise ValidationException(f'Key --> "{target_name}".    {target_name} must be provided')
     
     if doer == target:
-        return None, Response({'error': f'"{action}" failed.    {doer_name} and {target_name} cannot be the same'}, status=status.HTTP_400_BAD_REQUEST)
+        raise ValidationException(f'"{action}" failed.    {doer_name} and {target_name} cannot be the same')
     
     return doer, target
