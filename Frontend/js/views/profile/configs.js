00bde7a6373bb1b9e58b291cdcc75565ee672d52
FInacio97 <xico.inacio26@gmail.com>
2024-10-10 10:06:49 +0100

    still doesnt work

--- Frontend/js/views/profile/configs.js
+++ Frontend/js/views/profile/configs.js
@@ -25,25 +25,9 @@ export default {
             console.log("yoo");
             console.log(this.routeParams.id);
             console.log(`/user/profile/${this.routeParams.id}`);
-            // call(`/user/profile/${this.routeParams.id}`, 'GET').then((res)=>{
-            //     console.log(res);
-            // })
-            fetch(`/user/profile/${this.routeParams.id}`, {
-                method: 'GET', // Explicitly specify the method (optional since GET is default)
-            })
-              .then(response => {
-                if (!response.ok) {
-                  throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json(); // Parse the response as JSON
-              })
-              .then(data => {
-                console.log(data); // Handle the parsed data
-              })
-              .catch(error => {
-                console.error('Error:', error); // Handle errors
-              });
-            
+            call(`/user/profile/${this.routeParams.id}`, 'GET').then((res)=>{
+                console.log(res);
+            })            
         },
     }
 }
