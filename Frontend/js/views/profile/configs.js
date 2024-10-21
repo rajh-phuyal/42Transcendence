89d6ee7bb8d2f8e16186a9714b6e9147e919f81a
FInacio97 <xico.inacio26@gmail.com>
2024-10-21 12:45:38 +0100

    added some parameters to the profile view

--- Frontend/js/views/profile/configs.js
+++ Frontend/js/views/profile/configs.js
@@ -25,9 +25,16 @@ export default {
 
         afterDomInsertion() {
             call(`user/profile/${this.routeParams.id}/`, "GET").then((res)=>{
-                console.log(res);
                 let username = $id("username");   
                 username.textContent = "Subject: " + res.username;
+                let birthName =$id("birth-name");
+                birthName.textContent = "Birth name: " + res.lastName + ", " + res.firstName;
+                let lastSeenText =$id("last-seen-text");
+                lastSeenText.textContent = "Last seen: 2024.03.12 12:23" // + res.lastLogin;
+                let lastSeenImg =$id("last-seen-image");
+                lastSeenImg.src = "../../../../assets/onlineIcon.png";
+                let language =$id("language");
+                language.textContent = "language: " + res.language;
             })
             // on error?
         },
