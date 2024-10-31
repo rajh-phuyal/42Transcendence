e1df117bec482a03b9dd8ecee1c98856eed5371a
ahokcool <stein.alexander@posteo.de>
2024-10-31 13:29:18 +0100

    deleted file 'Backend/src/chat/routing.py'

--- Backend/src/chat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import consumers
-
-# Below the ws is choosen because django channels will automatically use wss for secure connections
-websocket_urlpatterns = [
-    path('ws/chat/', consumers.ChatConsumer.as_asgi()),  # WebSocket URL for chat
-]
