34eb2b4ca23701462656dc368a7b7176ff733520
ahokcool <stein.alexander@posteo.de>
2024-11-06 15:37:47 +0100

    dummy script now calls the root_accounts.sh. so I guess all should work. I also updated the dummy script to use the right user ids for our 5 dummy account (instead of 1-5 now 3-7)

--- Database/Postgres/tools/create_dummy.sh
+++ Database/Postgres/tools/create_dummy.sh
@@ -68,36 +68,48 @@ for table in "${ALL_TABLES[@]}"; do
     delete_old_data "$table"
 done
 
+# Important here we need to create the root users first!
+print_header "INSERTING ROOT USERS..."
+./usr/local/bin/root_accounts.sh
+
 print_header "INSERTING DUMMY DATA..."
 TABLE_NAME="barelyaschema.user"
 insert_dummy "$TABLE_NAME" \
 	"INSERT INTO $TABLE_NAME (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, avatar_path) VALUES \
-        (1, 'hashed_password_1', '2024-01-01 10:42:00+00', FALSE, 'arabelo-', 'Alê', 'Guedes', 'we dont use email', FALSE, FALSE, '2001-09-01 10:15:30+00', 'arabelo-.jpg'), \
-        (2, 'hashed_password_2', '2024-02-01 11:42:01+00', FALSE, 'astein', 'Alex', 'Stein', 'we dont use email', FALSE, FALSE, '2002-09-01 10:15:30+00', 'astein.jpg'), \
-        (3, 'hashed_password_3', '2024-03-01 12:42:02+00', FALSE, 'anshovah', 'Anatolii', 'Shovah', 'we dont use email', FALSE, FALSE, '2003-09-01 10:15:30+00', 'anshovah.jpg'), \
-        (4, 'hashed_password_4', '2024-04-01 13:42:03+00', FALSE, 'fda-estr', 'Francisco', 'Inácio', 'we dont use email', FALSE, FALSE, '2004-09-01 10:15:30+00', 'fda-estr.jpg'), \
-        (5, 'hashed_password_5', '2024-05-01 14:42:04+00', FALSE, 'rphuyal', 'Rajh', 'Phuyal', 'we dont use email', FALSE, FALSE, '2005-09-01 10:15:30+00', 'rphuyal.jpg');"
+        (3, 'hashed_password_1', '2024-01-01 10:42:00+00', FALSE, 'arabelo-', 'Alê', 'Guedes', 'we dont use email', FALSE, FALSE, '2001-09-01 10:15:30+00', 'arabelo-.jpg'), \
+        (4, 'hashed_password_2', '2024-02-01 11:42:01+00', FALSE, 'astein', 'Alex', 'Stein', 'we dont use email', FALSE, FALSE, '2002-09-01 10:15:30+00', 'astein.jpg'), \
+        (5, 'hashed_password_3', '2024-03-01 12:42:02+00', FALSE, 'anshovah', 'Anatolii', 'Shovah', 'we dont use email', FALSE, FALSE, '2003-09-01 10:15:30+00', 'anshovah.jpg'), \
+        (6, 'hashed_password_4', '2024-04-01 13:42:03+00', FALSE, 'fda-estr', 'Francisco', 'Inácio', 'we dont use email', FALSE, FALSE, '2004-09-01 10:15:30+00', 'fda-estr.jpg'), \
+        (7, 'hashed_password_5', '2024-05-01 14:42:04+00', FALSE, 'rphuyal', 'Rajh', 'Phuyal', 'we dont use email', FALSE, FALSE, '2005-09-01 10:15:30+00', 'rphuyal.jpg');"
 
-# TODO: add realtions to AI and the OVERLOARDS!
 TABLE_NAME="barelyaschema.is_cool_with"
 insert_dummy "$TABLE_NAME" \
 	"INSERT INTO $TABLE_NAME (id, requester_id, requestee_id, status) VALUES \
-		(1, 1, 2, 'accepted'), \
-		(2, 1, 3, 'accepted'), \
-		(3, 1, 4, 'accepted'), \
-		(4, 1, 5, 'accepted'), \
-		(5, 2, 3, 'accepted'), \
-		(6, 2, 4, 'pending'), \
-		(7, 5, 4, 'pending'), \
-		(8, 2, 5, 'rejected');"
-
+		(1, 2, 3, 'accepted'), \
+		(2, 2, 4, 'accepted'), \
+		(3, 2, 5, 'accepted'), \
+		(4, 2, 6, 'accepted'), \
+		(5, 2, 7, 'accepted'), \
+		(6, 3, 4, 'accepted'), \
+		(7, 3, 5, 'accepted'), \
+		(8, 3, 6, 'accepted'), \
+		(9, 3, 7, 'accepted'), \
+		(10, 4, 5, 'accepted'), \
+		(11, 4, 6, 'pending'), \
+		(12, 7, 6, 'pending'), \
+		(13, 4, 7, 'rejected');"
 
 TABLE_NAME="barelyaschema.no_cool_with"
 insert_dummy "$TABLE_NAME" \
 	"INSERT INTO $TABLE_NAME (id, blocker_id, blocked_id) VALUES \
-		(1, 3, 1), \
-		(2, 3, 4), \
-		(3, 4, 3);"
+		(1, 1, 3), \
+		(2, 1, 4), \
+		(3, 1, 5), \
+		(4, 1, 6), \
+		(5, 1, 7), \
+		(6, 5, 3), \
+		(7, 5, 6), \
+		(8, 6, 5);"
 
 # Conversation 1: 1-2
 # Conversation 2: 1-3
@@ -116,36 +128,40 @@ insert_dummy "$TABLE_NAME" \
 	"INSERT INTO $TABLE_NAME (id, conversation_id, user_id) VALUES \
 		(1, 1, 1), \
 		(2, 1, 2), \
-		(3, 2, 1), \
-		(4, 2, 3), \
-		(5, 3, 1), \
-		(6, 3, 2), \
-		(7, 3, 3), \
-		(8, 4, 1), \
-		(9, 4, 2), \
-		(10, 4, 3), \
-		(11, 4, 4), \
-		(12, 4, 5);"
+		(3, 1, 3), \
+		(4, 1, 4), \
+		(5, 1, 3), \
+		(6, 1, 4), \
+		(7, 2, 3), \
+		(8, 2, 5), \
+		(9, 3, 3), \
+		(10, 3, 4), \
+		(11, 3, 5), \
+		(12, 4, 3), \
+		(13, 4, 4), \
+		(14, 4, 5), \
+		(15, 4, 6), \
+		(16, 4, 7);"
 
 TABLE_NAME="barelyaschema.message"
 insert_dummy "$TABLE_NAME" \
 	"INSERT INTO $TABLE_NAME (id, user_id, conversation_id, content, created_at, seen_at) VALUES \
-		(1, 1, 1, 'Hi Alex, how are you?', '2024-01-01 10:42:00+00', '2024-01-01 10:42:00+00'), \
-		(2, 2, 1, 'Hi Alê, I am fine, thank you. How are you?', '2024-01-01 10:42:01+00', '2024-01-01 10:42:01+00'), \
-		(3, 1, 1, 'I am fine too, thank you.', '2024-01-01 10:42:02+00', '2024-01-01 10:42:02+00'), \
-		(4, 1, 2, 'Hi Anatolii, how are you?', '2024-01-01 10:42:03+00', '2024-01-01 10:42:03+00'), \
-		(5, 3, 2, 'Hi Alê, I am fine, thank you. How are you?', '2024-01-01 10:42:04+00', '2024-01-01 10:42:04+00'), \
-		(6, 1, 2, 'I am fine too, thank you.', '2024-01-01 10:42:05+00', '2024-01-01 10:42:05+00'), \
-		(7, 1, 3, 'Lets play this tournament', '2024-01-01 10:42:06+00', NULL), \
-		(8, 1, 3, 'Someone in this conversation???', '2024-01-01 10:45:06+00', NULL), \
-		(9, 1, 3, 'Booooriiiiinnggg!!!', '2024-01-01 10:55:06+00', NULL), \
-		(10, 2, 4, 'This is the scrum roooooom!! Are u guys here?', '2024-01-01 10:42:06+00', NULL), \
-		(11, 1, 4, 'Hello :)', '2024-01-01 10:43:06+00', NULL), \
-		(12, 3, 4, 'Yes, I am here', '2024-01-01 10:44:06+00', NULL), \
-		(13, 4, 4, 'I am here too', '2024-01-01 10:44:10+00', NULL), \
-		(14, 2, 4, 'Oye oye, what about u @rphuyal?', '2024-01-01 10:50:06+00', NULL), \
-		(15, 5, 4, 'Me as well', '2024-01-01 10:51:06+00', NULL), \
-		(16, 1, 4, 'Ok lets do this!', '2024-01-01 10:52:56+00',NULL);"
+		(1, 3, 1, 'Hi Alex, how are you?', '2024-01-01 10:42:00+00', '2024-01-01 10:42:00+00'), \
+		(2, 4, 1, 'Hi Alê, I am fine, thank you. How are you?', '2024-01-01 10:42:01+00', '2024-01-01 10:42:01+00'), \
+		(3, 3, 1, 'I am fine too, thank you.', '2024-01-01 10:42:02+00', '2024-01-01 10:42:02+00'), \
+		(4, 3, 2, 'Hi Anatolii, how are you?', '2024-01-01 10:42:03+00', '2024-01-01 10:42:03+00'), \
+		(5, 5, 2, 'Hi Alê, I am fine, thank you. How are you?', '2024-01-01 10:42:04+00', '2024-01-01 10:42:04+00'), \
+		(6, 3, 2, 'I am fine too, thank you.', '2024-01-01 10:42:05+00', '2024-01-01 10:42:05+00'), \
+		(7, 3, 3, 'Lets play this tournament', '2024-01-01 10:42:06+00', NULL), \
+		(8, 3, 3, 'Someone in this conversation???', '2024-01-01 10:45:06+00', NULL), \
+		(9, 3, 3, 'Booooriiiiinnggg!!!', '2024-01-01 10:55:06+00', NULL), \
+		(10, 4, 4, 'This is the scrum roooooom!! Are u guys here?', '2024-01-01 10:42:06+00', NULL), \
+		(11, 3, 4, 'Hello :)', '2024-01-01 10:43:06+00', NULL), \
+		(12, 5, 4, 'Yes, I am here', '2024-01-01 10:44:06+00', NULL), \
+		(13, 6, 4, 'I am here too', '2024-01-01 10:44:10+00', NULL), \
+		(14, 4, 4, 'Oye oye, what about u @rphuyal?', '2024-01-01 10:50:06+00', NULL), \
+		(15, 7, 4, 'Me as well', '2024-01-01 10:51:06+00', NULL), \
+		(16, 3, 4, 'Ok lets do this!', '2024-01-01 10:52:56+00',NULL);"
 
 print_header "RESETING SEQUENCES..."
 for table in "${ALL_TABLES[@]}"; do
--- Database/Postgres/tools/root_accounts.sh
+++ Database/Postgres/tools/root_accounts.sh
@@ -1,5 +1,5 @@
 # This file will deal with our 2 root user account:
-#     1. THE OVERLOARDS
+#     1. THE OVERLORDS
 #     2. THE AI OPPONENT
 #
 # It will be called by the entrypoint script of the postgres container.
@@ -9,11 +9,11 @@
 #	1 and 2. If they are not, it will throw an error.
 #
 # VARIABLES:
-OVERLOARDS_ID=1
-OVERLOARDS_USERNAME="overloards"
-OVERLOARDS_FIRST_NAME="The"
-OVERLOARDS_LAST_NAME="Overloards"
-OVERLOARDS_AVATAR="overloards.png"
+OVERLORDS_ID=1
+OVERLORDS_USERNAME="overlords"
+OVERLORDS_FIRST_NAME="The"
+OVERLORDS_LAST_NAME="Overlords"
+OVERLORDS_AVATAR="overlords.png"
 AI_ID=2
 AI_USERNAME="ai"
 AI_FIRST_NAME="The"
@@ -47,14 +47,18 @@ echo "STARTING ROOT ACCOUNTS SCRIPT..."
 if psql -U "$POSTGRES_USER" -d "$DB_NAME" -c "SELECT * FROM barelyaschema.user;" | grep -q "(0 rows)"; then
 	echo "Database is empty therfore create the root accounts..."
 	psql -U "$POSTGRES_USER" -d "$DB_NAME" -c "INSERT INTO barelyaschema.user (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, avatar_path) VALUES \
-		($OVERLOARDS_ID, 'hashed_password_1', '2000-01-01 00:00:00+00', TRUE, '$OVERLOARDS_USERNAME', '$OVERLOARDS_FIRST_NAME', '$OVERLOARDS_LAST_NAME', 'we dont use email', TRUE, TRUE, '2000-01-01 00:00:00+00', '$OVERLOARDS_AVATAR'), \
+		($OVERLORDS_ID, 'hashed_password_1', '2000-01-01 00:00:00+00', TRUE, '$OVERLORDS_USERNAME', '$OVERLORDS_FIRST_NAME', '$OVERLORDS_LAST_NAME', 'we dont use email', TRUE, TRUE, '2000-01-01 00:00:00+00', '$OVERLORDS_AVATAR'), \
 		($AI_ID, 'hashed_password_2', '2000-01-01 00:00:00+00', TRUE, '$AI_USERNAME', '$AI_FIRST_NAME', '$AI_LAST_NAME', 'we dont use email', TRUE, TRUE, '2000-01-01 00:00:00+00', '$AI_AVATAR');"
+	echo -e "Database is empty therfore create the root accounts..."$GR" DONE" $NC	
+	echo "Now resetting the sequences..."
+	psql -U "$POSTGRES_USER" -d "$DB_NAME" -c "SELECT setval('barelyaschema.user_id_seq', COALESCE((SELECT MAX(id) FROM barelyaschema.user), 1) + 1, false);"
+	echo -e "Now resetting the sequences..."$GR" DONE" $NC
 	end_script 0
 fi
 
 # Check if account overlords has the correct id and username
-if psql -U "$POSTGRES_USER" -d "$DB_NAME" -c "SELECT * FROM barelyaschema.user WHERE id = $OVERLOARDS_ID AND username = '$OVERLOARDS_USERNAME';" | grep -q "(0 rows)"; then
-	echo -e $RD "FATAL: Account id $OVERLOARDS_ID does not belong to the overloards, since the username is not '$OVERLOARDS_USERNAME'..."  $NC
+if psql -U "$POSTGRES_USER" -d "$DB_NAME" -c "SELECT * FROM barelyaschema.user WHERE id = $OVERLORDS_ID AND username = '$OVERLORDS_USERNAME';" | grep -q "(0 rows)"; then
+	echo -e $RD "FATAL: Account id $OVERLORDS_ID does not belong to the overlords, since the username is not '$OVERLORDS_USERNAME'..."  $NC
 	end_script 1
 fi
 
