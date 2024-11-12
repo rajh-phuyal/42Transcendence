ccdb6ebf9f097f6231488a22789f745214e62231
ahokcool <stein.alexander@posteo.de>
2024-11-12 21:27:28 +0100

    managed to fix issue #134

--- Backend/src/user/serializers.py
+++ Backend/src/user/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User
 from .utils import get_relationship_status
+from django.core.cache import cache
 
 # class UserSerializer(serializers.ModelSerializer):
 #     class Meta:
@@ -14,9 +15,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     avatarUrl = serializers.CharField(source='avatar_path', default='default_avatar.png')
     firstName = serializers.CharField(source='first_name', default="John")
     lastName = serializers.CharField(source='last_name', default="Doe")
-    online = serializers.BooleanField(default=False)
-    lastLogin = serializers.CharField(default="YYYY-MM-DD hh:mm")
-    language = serializers.CharField(default="en")
+    online = serializers.SerializerMethodField()
+    lastLogin = serializers.SerializerMethodField()
     chatId = serializers.CharField(default=42)
     newMessage = serializers.BooleanField(default=True)
     
@@ -27,9 +27,23 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-				# id and username are the same key than in the model.py, thats why i dont need them in the above section
         fields = ['id', 'username', 'avatarUrl', 'firstName', 'lastName', 'online', 'lastLogin', 'language', 'chatId', 'newMessage', 'relationship', 'stats']
-    
+
+    def get_lastLogin(self, obj):
+        # Check if `last_login` is None or `online` is True
+        if obj.last_login is None or self.get_online(obj):
+            return "under surveillance"
+
+        # Otherwise, format `last_login` as 'YYYY-MM-DD hh:mm'
+        return obj.last_login.strftime("%Y-%m-%d %H:%M")
+
+    def get_online(self, obj):
+        # AI Opponent and Overlords are always online
+        if obj.id == 1 or obj.id == 2:
+            return True
+        # Check if the user's online status is in the cache
+        return cache.get(f'user_online_{obj.id}', False)
+
     # Valid types are 'yourself' 'noFriend', 'friend', 'requestSent', 'requestReceived'
     def get_relationship(self, obj):
         # `requester` is the current authenticated user
