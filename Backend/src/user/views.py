0a249e59a04baf69ae8cb3f2a97285cc49df7870
rajh-phuyal <rajhphuyal44@gmail.com>
2024-11-21 22:04:09 +0000

    fixed small import error

--- Backend/src/user/views.py
+++ Backend/src/user/views.py
@@ -1,3 +1,4 @@
+from core.decorators import barely_handle_exceptions
 from core.base_views import BaseAuthenticatedView
 from core.response import success_response, error_response
 from django.utils.translation import gettext as _, activate
