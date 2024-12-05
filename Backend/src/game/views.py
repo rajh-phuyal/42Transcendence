bf50a25ec0681323c13978eb4aa1c09a22b844f6
Alexander Stein <stein.alexander@posteo.de>
2024-12-05 20:00:24 +0100

    starting

--- Backend/src/game/views.py
+++ Backend/src/game/views.py
@@ -5,6 +5,8 @@ from user.models import User
 from game.models import Game, GameMember
 from django.utils.translation import gettext as _
 from core.decorators import barely_handle_exceptions
+from user.utils_relationship import is_blocking, are_friends
+from user.constants import USER_ID_AI
 # from django.db.models import Q
 
 class CreateGameView(BaseAuthenticatedView):
@@ -12,9 +14,10 @@ class CreateGameView(BaseAuthenticatedView):
     def post(self, request):
         # Get the user from the request
         user = request.user
-        map_number = request.data.get('map_number')
-        if not map_number in [0, 1, 2, 3, 4]:
-            return error_response(_("Invalid value for key 'map_number'"))
+        # Parse the input
+        map_number = request.data.get('mapNumber')
+        if not map_number in [1, 2, 3, 4]:
+            return error_response(_("Invalid value for key 'mapNumber' value (must be 1, 2, 3 or 4)"))
         powerups = request.data.get('powerups')
         if not powerups in ['true', 'false']:
             return error_response(_("Invalid value for key 'powerups' (must be 'true' or 'false')"))
@@ -22,27 +25,53 @@ class CreateGameView(BaseAuthenticatedView):
             powerups = True
         else:
             powerups = False
+        local_game = request.data.get('localGame')
+        if not local_game in ['true', 'false']:
+            return error_response(_("Invalid value for key 'localGame' (must be 'true' or 'false')"))
+        if local_game == 'true':
+            local_game = True
+        else:
+            local_game = False
+        if local_game:
+            # TODO: issue #211
+            return error_response(_("Local games are not supported yet"))
         opponent_id = request.data.get('opponent')
         if not opponent_id:
             return error_response(_("Missing key 'opponent'"))
-        opponent = User.objects.filter(id=opponent_id).first()
+        # Check if opponent exist
+        try:
+            opponent = User.objects.get(id=opponent_id)
+        except User.DoesNotExist:
+            return error_response(_('Opponent not found'))
+        # Check if opponent isn't urself, is ur friend and not blocking you
         if opponent == user:
             return error_response(_("You can't play against yourself"))
-        if not opponent:
-            return error_response(_('Opponent not found'))
+        if not are_friends(user, opponent):
+            return error_response(_("You can only play against your friends"))
+        if is_blocking(opponent, user):
+            return error_response(_("You are blocked by this user"))
+        # Check if opponent is AI
+        if opponent.id == USER_ID_AI:
+            # TODO: issue #210
+            return error_response(_("Not implemented yet"))
+        # Check if there is already a direct game between the user and the opponent
+        user_games = GameMember.objects.filter(
+            user=user.id,
+            game__tournament_id=None,
+            game__state__in=[Game.GameState.PENDING, Game.GameState.ONGOING, Game.GameState.PAUSED]
+        ).values_list('game_id', flat=True)
+        opponent_games = GameMember.objects.filter(
+            user=opponent.id,
+            game__tournament_id=None,
+            game__state__in=[Game.GameState.PENDING, Game.GameState.ONGOING, Game.GameState.PAUSED]
+        ).values_list('game_id', flat=True)
+        if user_games or opponent_games:
+            common_games = set(user_games).intersection(opponent_games)
+            if common_games:
+                game_id = common_games.pop()
+                return success_response(_('Game already exists'), **{'game_id': game_id})
 
-        # Check if there is already a game between the user and the opponent
-        # TODO: issue #185 (https://github.com/rajh-phuyal/42Transcendence/issues/185)
-        #current_game = Game.objects.filter(
-        #    game_members__user=user,  # Use 'game_members' because it's the related name for GameMember in User
-        #    game_members__user=opponent,
-        #    tournament_id__isnull=True,
-        #    state__ne=Game.GameState.FINISHED
-        #).first()
-        #if current_game:
-        #    logging.info(f'User {user.id} is already in a game with opponent {opponent.id}, game_id: {current_game.id}')
-        #    return error_response(_('You already have a game with this opponent'), **{'game_id': current_game.id})
-
+        # Create the game and the game members in a transaction
         with transaction.atomic():
             game = Game.objects.create(
                 map_number=map_number,
@@ -51,7 +80,7 @@ class CreateGameView(BaseAuthenticatedView):
             game_member_user = GameMember.objects.create(
                 game=game,
                 user=user,
-                local_game=False,
+                local_game=local_game,
                 powerup_big = powerups,
                 powerup_fast = powerups,
                 powerup_slow = powerups
@@ -59,7 +88,7 @@ class CreateGameView(BaseAuthenticatedView):
             game_member_opponent = GameMember.objects.create(
                 game=game,
                 user=opponent,
-                local_game=False,
+                local_game=local_game,
                 powerup_big = powerups,
                 powerup_fast = powerups,
                 powerup_slow = powerups
