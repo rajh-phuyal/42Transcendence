9cc9d4e2f0018ec7a57dd9d636513b28c4010ebd
Alex <114107898+ahokcool@users.noreply.github.com>
2024-12-10 18:15:19 +0100

    Enhancement/185 be endpoint game create (#212)
    No we can create games via API:
    `http://127.0.0.1:8000/game/create/`
    
    Required Body:
    ```JSON
    {
        "mapNumber": 1,
        "powerups": "true",
        "opponentId": "9",
        "localGame": "false"
    }
    ```
    
    The create process will check:
    - map number between 1-4
    - if opponent exist
    - if user is friend with opponent
    - if user id not blocked by opponent
    - if no DIRECT game is already existing in one of those states:
    `PENDING`, `ONGOING`, `PAUSED`
       - those games can't have a tournament ID
    - if a game already exist it returns success with the existing `gameId`
    
    And then creates:
    - 1x entry in game table
    - 2x entries in game_member table
    
    It returns out default response:
     ```JSON
    {
       "status": "success",
    "message": "Game created successfully", # This will already be
    translated
       "gameId": 4
    }
     ```
     
    To test create some games via FLASHPOST, therefore u also need to send
    and accept friend requests via flashpost.
     
     
     To be done:
     - #210
     - #211 
    If u try with this params the endpoint fails gracefully by sending a
    "not implemented yet" response.

--- Backend/src/user/exceptions.py
+++ Backend/src/user/exceptions.py
@@ -11,6 +11,15 @@ class ValidationException(BarelyAnException):
         status_code = status_code or self.status_code
         super().__init__(detail, status_code)
 
+class UserNotFound(BarelyAnException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('User not found')
+
+    def __init__(self, detail=None, status_code=None):
+        detail = detail or self.default_detail
+        status_code = status_code or self.status_code
+        super().__init__(detail, status_code)
+
 class BlockingException(BarelyAnException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = _('A blocking condition has been detected.')
