0e2ad5d5ca6b0d15c167cf7c3f0a3c93fb72f91c
FInacio97 <xico.inacio26@gmail.com>
2024-12-17 12:48:56 +0100

    tournament view test prototype for websockets

--- Frontend/js/views/tournament/configs.js
+++ Frontend/js/views/tournament/configs.js
@@ -1,9 +1,29 @@
+import call from '../../abstracts/call.js'
+
 export default {
     attributes: {
 
     },
 
     methods: {
+
+        createParticipantCard(userData) {
+
+            let card = this.domManip.$id("tournament-list-card-template").content.cloneNode(true);
+
+            // atripute grey background color if the user is not in the tournament yet
+            card.querySelector(".card").style.backgroundColor = "grey";
+            card.querySelector(".username").textContent = userData.username;
+
+        },
+
+        
+        buildParticipantsList(list) {
+            const mainDiv = this.domManip.$id("tournament-list");
+
+            for (element of list) 
+                mainDiv.appendChild(createParticipantCard(element));
+        }
 
     },
 
@@ -21,7 +41,20 @@ export default {
         },
 
         afterDomInsertion() {
+            const id = 1;
+            call(`tournament/lobby/${id}/`, 'GET').then(data => {
 
+                let stateColor;
+                if (data.tournament.state == "setup")
+                    stateColor = "red";
+                else
+                    stateColor = "green";
+
+                this.domManip.$id("status").style.backgroundColor = stateColor;
+                this.domManip.$id("tournament-name").textContent = data.tournamentName;
+                
+                
+            })
         },
     }
 }
