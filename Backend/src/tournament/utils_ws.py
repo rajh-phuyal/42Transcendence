6a8732c798ca723fc400989614f17272ede47dee
Alex <114107898+ahokcool@users.noreply.github.com>
2025-02-01 21:24:28 +0100

    tournament invite message format

--- Backend/src/tournament/utils_ws.py
+++ Backend/src/tournament/utils_ws.py
@@ -59,11 +59,14 @@ def send_tournament_invites_via_pm(tournament_id):
     for member in tournament_members:
         # Check if there's a private conversation between the user and the admin
         conversation = get_conversation(tournament_admin, member)
-        invite_message = _("The overloads spectace that the holly @{username_admin} has invited @{username_guest} to the fantastic tournament '{tournament_name}'"
+        invite_message = _("The overloads spectace that the holly @{admin_username}@{admin_id}@ has invited @{guest_username}@{guest_id}@ to the fantastic tournament #T#{tournament_name}#{tournament_id}#"
             ).format(
-                username_admin=tournament_admin.user.username,
-                username_guest=member.user.username,
-                tournament_name=tournament.name
+                admin_username=tournament_admin.user.username,
+                admin_id=tournament_admin.user.id,
+                guest_username=member.user.username,
+                guest_id=member.user.id,
+                tournament_name=tournament.name,
+                tournament_id=tournament.id
             )
 
         # If not, create one
