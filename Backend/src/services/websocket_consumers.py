2fb52608bce31d43fd5b60a47a38e8333af8accd
Rajh <81531002+rajh-phuyal@users.noreply.github.com>
2025-02-02 20:07:34 +0000

    [ongoing] Coheshive game merge (#300)
    This pr merges 3 parts for the game view: FE: Game loop, FE: Game render
    and BE:Game loop.
    
    ---------
    
    Co-authored-by: FInacio97 <xico.inacio26@gmail.com>
    Co-authored-by: AshParker19 <ashparker4219@gmail.com>
    Co-authored-by: rabeloguedes <rabeloguedes@proton.me>
    Co-authored-by: Alexander Stein <stein.alexander@posteo.de>

--- Backend/src/services/websocket_consumers.py
+++ Backend/src/services/websocket_consumers.py
@@ -1,3 +1,5 @@
+import asyncio
+import random
 from game.models import Game
 from asgiref.sync import sync_to_async
 import json
@@ -15,10 +17,13 @@ from core.exceptions import BarelyAnException
 from core.decorators import barely_handle_ws_exceptions
 from services.chat_service import setup_all_conversations, send_total_unread_counter
 from services.tournament_service import setup_all_tournament_channels
-from services.websocket_utils import WebSocketMessageHandlersMain, WebSocketMessageHandlersGame, parse_message
+from services.websocket_utils import WebSocketMessageHandlersMain, WebSocketMessageHandlersGame, send_player_ready_state, parse_message
 from channels.layers import get_channel_layer
 from channels.db import database_sync_to_async
-from datetime import datetime, timedelta
+from game.constants import GAME_FPS, PADDLE_OFFSET
+from game.utils_ws import init_game
+from game.utils_ws import update_game_state_db
+
 
 channel_layer = get_channel_layer()
 
@@ -56,14 +61,17 @@ class CustomWebSocketLogic(AsyncWebsocketConsumer):
             await self.close()
             raise BarelyAnException(_("User is not authenticated."))
         # Parse the message (only the messageType is required at this point)
-        self.message_type = parse_message(text_data, mandatory_keys=['messageType']).get('messageType')
-        logging.info(f"Received Websocket Message type: {self.message_type}")
+        self.message_type = parse_message(text_data, mandatory_keys=[
+                                          'messageType']).get('messageType')
+        # logging.info(f"Received Websocket Message type: {self.message_type}")
 
     def update_user_last_seen(self, user):
-        user.last_seen = timezone.now() #TODO: Issue #193
+        user.last_seen = timezone.now()  # TODO: Issue #193
         user.save(update_fields=['last_seen'])
 
 # Manages the WebSocket connection for all pages after login
+
+
 class MainConsumer(CustomWebSocketLogic):
     @barely_handle_ws_exceptions
     async def connect(self):
@@ -112,98 +120,79 @@ class MainConsumer(CustomWebSocketLogic):
 
     async def update_badge(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def new_conversation(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def tournament_subscription(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def tournament_state(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def info(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def game_create(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def game_set_deadline(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def game_update_score(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def game_update_state(self, event):
         await self.send(text_data=json.dumps({**event}))
+
     async def game_update_rank(self, event):
         await self.send(text_data=json.dumps({**event}))
 
 # Manages the temporary WebSocket connection for a single game
+
+
 class GameConsumer(CustomWebSocketLogic):
-    @barely_handle_ws_exceptions    
+    game_loops = {}
+
+    # @barely_handle_ws_exceptions
     async def connect(self):
         # TODO: check if the user is in the game
         await super().connect()
         # Add client to the channel layer group
-        game_id = self.scope['url_route']['kwargs']['game_id']
-        logging.info(f"Opening WebSocket connection for game {game_id} and user {self.scope['user']} ...")
+        self.game_id = self.scope['url_route']['kwargs']['game_id']
+        logging.info(
+            f"Opening WebSocket connection for game {self.game_id} and user {self.scope['user']} ...")
 
         # Set the player ready
-        game = await database_sync_to_async(Game.objects.get)(id=game_id)
+        game = await database_sync_to_async(Game.objects.get)(id=self.game_id)
         game.set_player_ready(self.scope['user'].id, True)
-        
-        game_name = f"game_{game_id}"
+
+        game_name = f"game_{self.game_id}"
         await channel_layer.group_add(game_name, self.channel_name)
-        
+
         # Accept the connection
         await self.accept()
 
-        game_user_ids = await database_sync_to_async(lambda: [player.user.id for player in list(game.game_members.all())])()
-        player_right = await sync_to_async(game.get_player_ready)(min(game_user_ids))
-        player_left = await sync_to_async(game.get_player_ready)(max(game_user_ids))
-        start_time = None
-        if player_left and player_right:
-            start_time = datetime.now() + timedelta(seconds=5)
-            start_time = start_time.isoformat()        
+        can_start_game, start_time = await send_player_ready_state(game, channel_layer)
 
-        await channel_layer.group_send(
-            game_name,
-            {
-                "type": "update_players_ready",
-                "messageType": "playersReady",
-                "playerLeft": player_left,
-                "playerRight": player_right,
-                "startTime": start_time 
-            }
-        )
-
+        logging.info(
+            f"WebSocket connection for game {self.game_id} and user {self.scope['user']} opened.")
+        if can_start_game:
+            await init_game(game)
+            GameConsumer.game_loops[self.game_id] = asyncio.create_task(
+                GameConsumer.run_game_loop(self.game_id, start_time))
 
     @barely_handle_ws_exceptions
     async def disconnect(self, close_code):
         await super().disconnect(close_code)
-        # Set the player ready
-        game_id = self.scope['url_route']['kwargs']['game_id']
-        game = await sync_to_async(Game.objects.get)(id=game_id)
+        # Set the player ready to false
+        self.game_id = self.scope['url_route']['kwargs']['game_id']
+        game = await sync_to_async(Game.objects.get)(id=self.game_id)
         game.set_player_ready(self.scope['user'].id, False)
 
-        # Doing game stuff
+        await send_player_ready_state(game, channel_layer)
 
-        game_name = f"game_{game_id}"
-        await channel_layer.group_add(game_name, self.channel_name)
-        
-        game_user_ids = await database_sync_to_async(lambda: [player.user.id for player in list(game.game_members.all())])()
-        player_right = await sync_to_async(game.get_player_ready)(min(game_user_ids))
-        player_left = await sync_to_async(game.get_player_ready)(max(game_user_ids))
-        start_time = None
-        if player_left and player_right:
-            start_time = datetime.now() + timedelta(seconds=5)
-            start_time = start_time.isoformat()        
-
-        await channel_layer.group_send(
-            game_name,
-            {
-                "type": "update_players_ready",
-                "messageType": "playersReady",
-                "playerLeft": player_left,
-                "playerRight": player_right,
-                "startTime": start_time 
-            }
-        )
-
-        ...
+        await update_game_state_db(game, Game.GameState.PAUSED)
 
     @barely_handle_ws_exceptions
     async def receive(self, text_data):
@@ -217,3 +206,174 @@ class GameConsumer(CustomWebSocketLogic):
     @barely_handle_ws_exceptions
     async def update_players_ready(self, event):
         await self.send(text_data=json.dumps({**event}))
+
+    @barely_handle_ws_exceptions
+    async def update_game_state(self, event):
+        await self.send(text_data=json.dumps({**event}))
+
+    @staticmethod
+    async def run_game_loop(game_id, start_time):
+        while True:
+            try:
+                # Fetches user desires from cache
+                left_player_input = cache.get(f'game_{game_id}_player_left', {})
+                right_player_input = cache.get(f'game_{game_id}_player_right', {})
+
+                # Fetches game state from cache
+                game_state_data = cache.get(f'game_{game_id}_state', {})
+
+                # Extract user state from game state
+                game_state_data_left = game_state_data['playerLeft']
+                game_state_data_right = game_state_data['playerRight']
+
+                # Change postions of both paddles if requested and allowed
+                game_state_data_left = move_paddle(left_player_input, game_state_data_left)
+                game_state_data_right = move_paddle(right_player_input, game_state_data_right)
+
+                # Update game state with new paddle positions
+                game_state_data['playerLeft'] = game_state_data_left
+                game_state_data['playerRight'] = game_state_data_right
+
+                # Then calculate ball movement
+                game_state_data = move_ball(game_state_data)
+
+                # Send the updated game state to FE
+                game_name = f"game_{game_id}"
+                await channel_layer.group_send(
+                    game_name,
+                    {
+                        "type": "update_game_state",
+                        "messageType": "gameState",
+                        **game_state_data
+                    }
+                )
+
+                # Store the updated game state in cache
+                cache.set(f'game_{game_id}_state', game_state_data, timeout=3000)
+
+                # Check if the game is over and exit the loop
+                if game_state_data['gameData']['state'] == 'finished':
+                    break
+
+                # Await for next frame render
+                await asyncio.sleep(1 / GAME_FPS)
+            except Exception as e:
+                logging.error(f"Error in game loop: {e}")
+                break
+        logging.info(f"Game finished: {game_id}")
+
+
+def move_paddle(player, game_state_data_player):
+    # logging.info(f"paddlePos for {player['movePaddle']}: {game_state_data_player['paddlePos']}")
+
+    if player['movePaddle'] == '0':
+        return game_state_data_player
+
+    logging.info(f"player['movePaddle']: player['movePaddle']")
+
+    new_paddle_pos = game_state_data_player['paddlePos']
+    if player['movePaddle'] == '+':
+        new_paddle_pos += game_state_data_player['paddleSpeed']
+    elif player['movePaddle'] == '-':
+        new_paddle_pos -= game_state_data_player['paddleSpeed']
+
+    if (new_paddle_pos - (game_state_data_player['paddleSize'] / 2)) < 0:
+        new_paddle_pos = game_state_data_player['paddleSize'] / 2
+
+    elif (new_paddle_pos + (game_state_data_player['paddleSize'] / 2)) > 100:
+        new_paddle_pos = 100 - (game_state_data_player['paddleSize'] / 2)
+    game_state_data_player['paddlePos'] = new_paddle_pos
+    return game_state_data_player
+
+
+def move_ball(game_state_data):
+    #logging.info(f"ballPos:\t x --> {game_state_data['gameData']['ballPosX']} \t y --> {game_state_data['gameData']['ballPosY']} \t Players pos: left --> {game_state_data['playerLeft']['paddlePos']} {game_state_data['playerRight']['paddlePos']}")
+
+    # Move the ball
+    game_state_data['gameData']['ballPosX'] += game_state_data['gameData']['ballDirectionX'] * game_state_data['gameData']['ballSpeed']
+    game_state_data['gameData']['ballPosY'] += game_state_data['gameData']['ballDirectionY'] * game_state_data['gameData']['ballSpeed']
+
+    # Check if the ball is hitting the left or right paddle(point was scored)
+    if game_state_data['gameData']['ballPosX'] <= PADDLE_OFFSET + game_state_data['gameData']['ballRadius']:
+        game_state_data['gameData']['ballPosX'] = PADDLE_OFFSET + game_state_data['gameData']['ballRadius']
+        game_state_data['gameData']['ballDirectionX'] *= -1
+        game_state_data = check_padlle_hit(game_state_data, 'playerLeft')
+    elif game_state_data['gameData']['ballPosX'] >= 100 - PADDLE_OFFSET - game_state_data['gameData']['ballRadius']:
+        game_state_data['gameData']['ballPosX'] = 100 - PADDLE_OFFSET - game_state_data['gameData']['ballRadius']
+        game_state_data['gameData']['ballDirectionX'] *= -1
+        game_state_data = check_padlle_hit(game_state_data, 'playerRight')
+
+    # Calculate if the upper or lower wall was hit
+    if game_state_data['gameData']['ballPosY'] <= game_state_data['gameData']['ballRadius']:
+        game_state_data['gameData']['ballPosY'] = game_state_data['gameData']['ballRadius']
+        game_state_data['gameData']['ballDirectionY'] *= -1
+    elif game_state_data['gameData']['ballPosY'] >= 100 - game_state_data['gameData']['ballRadius']:
+        game_state_data['gameData']['ballPosY'] = 100 - game_state_data['gameData']['ballRadius']
+        game_state_data['gameData']['ballDirectionY'] *= -1
+
+    return game_state_data
+
+def check_padlle_hit(game_state_data, player_side):
+    # Check if a point was scored
+    # if ballPos < paddlePos
+    if game_state_data['gameData']['ballPosY'] < game_state_data[player_side]['paddlePos']:
+    #     if ballPos + ballRadius < paddlePos - paddleSize / 2
+        if game_state_data['gameData']['ballPosY'] + game_state_data['gameData']['ballRadius'] < game_state_data[player_side]['paddlePos'] - game_state_data[player_side]['paddleSize'] / 2:
+    #         score a point
+            game_state_data = score_point(game_state_data, player_side)
+        else:
+            # Ball should bounce off up
+    #         distance_paddle_ball = paddlePos - ballPos
+            distance_paddle_ball = game_state_data[player_side]['paddlePos'] - game_state_data['gameData']['ballPosY']
+    #         percentage_y = distance_paddle_ball / (paddleSize / 2 + ballRadius)
+            percentage_y = distance_paddle_ball / (game_state_data[player_side]['paddleSize'] / 2 + game_state_data['gameData']['ballRadius'])
+    #         ballDirectionY = -percentage_y
+            game_state_data['gameData']['ballDirectionY'] = -percentage_y
+    else:
+    #     if ballPos - ballRadius > paddlePos + paddleSize / 2
+        if game_state_data['gameData']['ballPosY'] - game_state_data['gameData']['ballRadius'] > game_state_data[player_side]['paddlePos'] + game_state_data[player_side]['paddleSize'] / 2:
+    #         score a point
+            game_state_data = score_point(game_state_data, player_side)
+        else:
+    #         # Ball should bounce off down
+    #         distance_paddle_ball = ballPos - paddlePos
+            distance_paddle_ball = game_state_data['gameData']['ballPosY'] - game_state_data[player_side]['paddlePos']
+    #         percentage_y = distance_paddle_ball / (paddleSize / 2 + ballRadius)
+            percentage_y = distance_paddle_ball / (game_state_data[player_side]['paddleSize'] / 2 + game_state_data['gameData']['ballRadius'])
+    #         ballDirectionY = percentage_y
+            game_state_data['gameData']['ballDirectionY'] = percentage_y
+
+    return game_state_data
+
+
+def score_point(game_state_data, player_side):
+    game_state_data['gameData']['ballPosX'] = 50
+    game_state_data['gameData']['ballPosY'] = 50
+    game_state_data['gameData']['ballSpeed'] = 1
+
+    if (game_state_data['gameData']['remainingServes'] > 0):
+        game_state_data['gameData']['remainingServes'] -= 1
+    else:
+        game_state_data['gameData']['remainingServes'] = 2
+        if (game_state_data['gameData']['playerServes'] == 'playerLeft'):
+            game_state_data['gameData']['playerServes'] = 'playerRight'
+        else:
+            game_state_data['gameData']['playerServes'] = 'playerLeft'
+
+    if (game_state_data['gameData']['playerServes'] == 'playerLeft'):
+        game_state_data['gameData']['ballDirectionX'] = -1
+    else:
+        game_state_data['gameData']['ballDirectionX'] = 1
+    # Setting dir to a random value
+    game_state_data['gameData']['ballDirectionY'] = random.uniform(-0.01, 0.01)
+
+    # Update the score
+    if (player_side == 'playerLeft'):
+        game_state_data['playerLeft']['points'] += 1 # TODO: HACKATHON Update db
+    else:
+        game_state_data['playerRight']['points'] += 1 # TODO: HACKATHON Update db
+
+    if (game_state_data['playerLeft']['points'] >= 11 or game_state_data['playerRight']['points'] >= 11):
+        game_state_data['gameData']['state'] = 'finished' # TODO: HACKATHON Update db
+
+    return game_state_data
--- Frontend/js/views/game/methods.js
+++ Frontend/js/views/game/methods.js
@@ -1,21 +1,242 @@
-import { $id } from '../../abstracts/dollars.js';
-import { translate } from '../../locale/locale.js';
+import { $id, $on, $off, $class } from '../../abstracts/dollars.js';
+import { gameObject } from './objects.js';
+import WebSocketManagerGame from '../../abstracts/WebSocketManagerGame.js';
+import { gameRender } from './render.js';
 
 export function changeGameState(state) {
     switch (state) {
         case "ongoing": // transition lobby to game
             $id("button-quit-game").style.display = "none";
-            return ;
+            return;
         case "paused": // transition game to lobby
             $id("button-quit-game").style.display = "none";
-            return ;
+            return;
         case "finished": // transition game to lobby
             $id("button-quit-game").style.display = "none";
-            return ;
+            return;
     }
     console.warn("FE doen't know what to do with this state:", state);
 }
 
+// function logTimeWithMilliseconds() {
+//     const now = new Date();
+//     const hours = now.getHours().toString().padStart(2, '0');
+//     const minutes = now.getMinutes().toString().padStart(2, '0');
+//     const seconds = now.getSeconds().toString().padStart(2, '0');
+//     const milliseconds = now.getMilliseconds().toString().padStart(3, '0');
+
+//     console.log(`${hours}:${minutes}:${seconds}.${milliseconds}`);
+// }
+
+function updatePlayerInput() {
+    // Send the ws message to the server
+    const message = {
+        messageType: "playerInput",
+        playerLeft: {
+        movePaddle: gameObject.playerInput.paddleMovement || "0",
+        powerupBig: gameObject.playerInput.powerupBig || false,
+        powerupSlow: gameObject.playerInput.powerupSlow || false,
+        powerupFast: gameObject.playerInput.powerupFast || false
+        }
+        // TODO: add player right
+    }
+    WebSocketManagerGame.sendMessage(message);
+}
+
+function gameLoop(currentTime) {
+    if (currentTime - gameObject.lastFrameTime >= 30) {
+
+        // Check if the game is ongoing
+        if (gameObject.state !== "ongoing") {
+            cancelAnimationFrame(gameObject.animationId);
+            return;
+        }
+        // Render the game
+        // console.log("player1:", gameObject.playerLeft.pos);
+        // console.log("player2:", gameObject.playerRight.pos);
+
+        updatePlayerInput();
+        updatePowerupStatus();
+        gameRender();
+
+        gameObject.lastFrameTime = currentTime;
+    }
+    gameObject.animationId = requestAnimationFrame(gameLoop);
+}
+
+function keyPressCallback(event) {
+    switch (event.key) {
+        case "w":
+            // Move the paddle up
+            gameObject.playerInput.paddleMovement = "-";
+            break;
+        case "s":
+            // Move the paddle down
+            gameObject.playerInput.paddleMovement = "+";
+            break;
+        case "1":
+            // Activate the powerup
+            gameObject.playerInput.powerupBig = true;
+            break;
+        case "2":
+            // Activate the powerup
+            gameObject.playerInput.powerupSlow = true;
+            break;
+        case "3":
+            // Activate the powerup
+            gameObject.playerInput.powerupFast = true;
+            break;
+        default:
+            return;
+    }
+    updatePlayerInput();
+}
+
+function keyReleaseCallback(event) {
+    switch (event.key) {
+        case "w":
+            // Stop the paddle
+            gameObject.playerInput.paddleMovement = "0";
+            break;
+        case "s":
+            // Stop the paddle
+            gameObject.playerInput.paddleMovement = "0";
+            break;
+    }
+    updatePlayerInput();
+}
+
+const percentageToPixels = (percentage, edgeSize) => {
+    return (edgeSize / 100) * percentage;
+}
+
+export function updateGameObjects(gameState) {
+    const gameField = $id("game-field");
+
+    gameObject.state = gameState?.gameData?.state;
+    gameObject.playerLeft.points = gameState?.playerLeft?.points;
+    gameObject.playerRight.points = gameState?.playerRight?.points;
+
+    gameObject.playerLeft.pos = percentageToPixels(gameState?.playerLeft?.paddlePos, gameField?.height);
+    gameObject.playerRight.pos = percentageToPixels(gameState?.playerRight?.paddlePos, gameField?.height);
+    gameObject.playerLeft.size = percentageToPixels(gameState?.playerLeft?.paddleSize, gameField?.height);
+    gameObject.playerRight.size = percentageToPixels(gameState?.playerRight?.paddleSize, gameField?.height);
+    gameObject.ball.posX = percentageToPixels(gameState?.gameData?.ballPosX, gameField?.width);
+    gameObject.ball.posY = percentageToPixels(gameState?.gameData?.ballPosY, gameField?.height);
+    gameObject.ball.size = 4;
+
+    gameObject.playerLeft.powerups.big = gameState?.playerLeft?.powerupBig;
+    gameObject.playerLeft.powerups.slow = gameState?.playerLeft?.powerupSlow;
+    gameObject.playerLeft.powerups.fast = gameState?.playerLeft?.powerupFast;
+    gameObject.playerRight.powerups.big = gameState?.playerRight?.powerupBig;
+    gameObject.playerRight.powerups.slow = gameState?.playerRight?.powerupSlow;
+    gameObject.playerRight.powerups.fast = gameState?.playerRight?.powerupFast;
+}
+
+function updatePowerupStatus() {
+    gameObject.playerLeft.powerups.big != "used" ? $id('player-left-powerups-big').style.color = "white" : $id('player-left-powerups-big').style.color = "gray";
+    gameObject.playerLeft.powerups.fast != "used" ? $id('player-left-powerups-fast').style.color = "white" : $id('player-left-powerups-fast').style.color = "gray";
+    gameObject.playerLeft.powerups.slow != "used" ? $id('player-left-powerups-slow').style.color = "white" : $id('player-left-powerups-slow').style.color = "gray";
+    gameObject.playerRight.powerups.big != "used" ? $id('player-right-powerups-big').style.color = "white" : $id('player-right-powerups-big').style.color = "gray";
+    gameObject.playerRight.powerups.fast != "used" ? $id('player-right-powerups-fast').style.color = "white" : $id('player-right-powerups-fast').style.color = "gray";
+    gameObject.playerRight.powerups.slow != "used" ? $id('player-right-powerups-slow').style.color = "white" : $id('player-right-powerups-slow').style.color = "gray";
+}
+
+function showPowerupStatus() {
+        let elements = $class('user-state')
+        for (let element of elements)
+            element.style.display = "none";
+
+         $id("player-left-state-spinner").style.display = "none";
+         $id("player-right-state-spinner").style.display = "none";
+
+        elements = $class('player-powerup-status')
+
+        for (let element of elements)
+            element.style.display = "flex";
+
+        updatePowerupStatus();
+}
+
+export function endGameLoop() {
+    cancelAnimationFrame(gameObject.animationId);
+    $off(document, 'keydown', keyPressCallback);
+    $off(document, 'keyup', keyReleaseCallback);
+}
+
+export function startGameLoop() {
+    $on(document, 'keydown', keyPressCallback);
+    $on(document, 'keyup', keyReleaseCallback);
+    gameObject.lastFrameTime = performance.now();
+    gameObject.animationId = requestAnimationFrame(gameLoop);
+    showPowerupStatus();
+
+}
+
+const animateImage = (
+    id,
+    animationName,
+    duration = "1s",
+    iterationCount = "1",
+    timingFunction = "ease-in-out"
+) => {
+    const image = $id(id);
+    //image.style.animationDuration = duration;
+    //image.style.animationName = animationName;
+    //image.style.animationIterationCount = iterationCount;
+    //image.style.animationTimingFunction = timingFunction;
+};
+
+const removeImageAnimation = (id) => {
+    const image = $id(id);
+    //image.style.animationDuration = "0s";
+    //image.style.animationName = "";
+    //image.style.animationIterationCount = "0";
+}
+
+
+const showGame = () => {
+    const gameField = $id("game-field");
+    const gameViewImageContainer = $id("game-view-image-container");
+    const gameImageContainer = $id("game-view-map-image");
+
+    const gameImage = gameImageContainer.children[0];
+
+    // TODO: add proper map image
+    gameImage.src = window.location.origin + '/assets/game/maps/ufo.png';
+
+    gameViewImageContainer.style.backgroundImage = "none";
+    gameViewImageContainer.style.width = "100%";
+    gameField.style.display = "block";
+
+    gameImage.style.display = "block";
+    animateImage("game-view-map-image", "fadein", "3s");
+}
+
+const countdownImageObject = {
+    0: "",
+    1: `${window.origin}/assets/game/countdown/countdown1.png`,
+    2: `${window.origin}/assets/game/countdown/countdown2.png`,
+    3: `${window.origin}/assets/game/countdown/countdown3.png`,
+    4: `${window.origin}/assets/game/countdown/countdown4.png`,
+    5: `${window.origin}/assets/game/countdown/countdown5.png`,
+    6: `${window.origin}/assets/game/countdown/countdown6.png`,
+    7: `${window.origin}/assets/game/countdown/countdown7.png`,
+    8: `${window.origin}/assets/game/countdown/countdown8.png`,
+    9: `${window.origin}/assets/game/countdown/countdown9.png`,
+}
+
+const gameCountdownImage = (currentTime) => {
+    const gameCountdownImage = $id("game-countdown-image");
+    gameCountdownImage.src = countdownImageObject[currentTime];
+    gameCountdownImage.style.display = "block";
+
+    if (!currentTime) {
+        gameCountdownImage.style.display = "none";
+    }
+}
+
+let gameCountdownIntervalId = undefined;
 export function updateReadyState(readyStateObject) {
 
     if (readyStateObject.playerLeft) {
@@ -36,6 +257,28 @@ export function updateReadyState(readyStateObject) {
         $id("player-right-state").style.display = "none";
     }
 
-    if (readyStateObject.startTime)
-        console.warn("Start Time:", readyStateObject.startTime);
+    if (readyStateObject.startTime) {
+        animateImage("game-countdown-image", "pulsate", "1s", "infinite");
+        gameCountdownIntervalId = setInterval((startTime) => {
+            let diff = Math.floor((startTime - new Date()) / 1000);
+            if (diff <= 0) {
+                clearInterval(gameCountdownIntervalId);
+                console.log("Starting game loop");
+                gameCountdownImage(0);
+                startGameLoop();
+                return;
+            }
+
+            if (diff == 3) {
+                console.log("Fading in map image");
+                showGame();
+                setTimeout(() => {
+                    removeImageAnimation("game-view-map-image");
+                }, 3000);
+            }
+
+            // update image here
+            gameCountdownImage(diff);
+        }, 1000, Date.parse(readyStateObject.startTime));
+    }
 }
