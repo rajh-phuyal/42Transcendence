76c8be38f60142cc775896f95b8fe1d2c05d7011
Alexander Stein <stein.alexander@posteo.de>
2025-02-16 19:51:21 +0100

    ...

--- Frontend/js/views/profile/methods.js
+++ Frontend/js/views/profile/methods.js
@@ -1,11 +1,4 @@
-// TODO: I guess we can delete this file?
-
 import router from '../../navigation/router.js';
-import { processIncomingWsChatMessage } from '../chat/methods.js';
-
-function openConversation() {
-    router("/chat");
-}
 
 export function processIncomingReloadMsg(msg, currentProfile) {
     const currentProfileId = currentProfile.split("-")[1];
@@ -13,5 +6,3 @@ export function processIncomingReloadMsg(msg, currentProfile) {
     if (currentProfileId == msg.userId)
         router("/profile", { id: msg.userId });
 }
-
-export { openConversation };
