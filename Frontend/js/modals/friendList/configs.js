865da8a2dc2bb3ddc7bf0cd86f361b4d5fd7ae95
Alexander Stein <stein.alexander@posteo.de>
2025-02-28 14:57:26 +0100

    added todos

--- Frontend/js/modals/friendList/configs.js
+++ Frontend/js/modals/friendList/configs.js
@@ -1,3 +1,14 @@
+/*
+TODO: THIS MODAL IS NOT DONE AT ALL!!!
+    NEED TO:
+        - copy the right structure from the template modal
+        - double check all nodes/elements if needed?
+        - adjust the js code
+            - move it from original configs.js (profile/home) to congigs.js of modal!
+            - make sure the js code has all values. the idea is that the view stores the info as attribute and the modal takes it from there
+            - e.g. newConversation modal js!
+*/
+
 import { modalManager } from '../../abstracts/ModalManager.js';
 
 export default {
