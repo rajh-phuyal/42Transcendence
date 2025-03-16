1e9b5f04ea3e6e6df38105c651fe5691f11afca9
FInacio97 <xico.inacio26@gmail.com>
2025-03-16 17:41:08 +0100

    game history is creating the game cards

--- Frontend/js/modals/gameHistory/configs.js
+++ Frontend/js/modals/gameHistory/configs.js
@@ -8,22 +8,78 @@ TODO: THIS MODAL IS NOT DONE AT ALL!!!
             - make sure the js code has all values. the idea is that the view stores the info as attribute and the modal takes it from there
             - e.g. newConversation modal js!
 */
-
+import call from '../../abstracts/call.js';
 import { modalManager } from '../../abstracts/ModalManager.js';
+import { createGameCard } from '../../views/tournament/methods.js';
 
 export default {
     attributes: {
-
+        userId: undefined,
+        data: undefined,
     },
 
     methods: {
+
+        cleanUpGameList() {
+            let list = this.domManip.$queryAll(".modal-game-history-card");
+
+            for (let element of list)
+                element.remove();
+        },
+
+        setAvatarOnGameCard (element, playerObject) {
+            element.src = `${window.origin}/media/avatars/${playerObject.avatarUrl}`;
+            if (playerObject.result === "lost")
+                element.style.filter = "brightness(50%)";
+        },
+
+        createGameCard(gameObject) {
+            const template = this.domManip.$id("modal-game-history-card-template").content.cloneNode(true);
+            const container = template.querySelector(".modal-game-history-card");
+
+            this.setAvatarOnGameCard(container.querySelector(".modal-game-history-card-playerLeft-avatar"), gameObject.playerLeft);
+            this.setAvatarOnGameCard(container.querySelector(".modal-game-history-card-playerRight-avatar"), gameObject.playerRight);
+
+            container.querySelector(".modal-game-history-card-playerLeft-username").textContent = gameObject.playerLeft.username;
+            container.querySelector(".modal-game-history-card-playerRight-username").textContent = gameObject.playerRight.username;
+
+            container.querySelector(".modal-game-history-card-score").textContent = `${gameObject.playerLeft.points}-${gameObject.playerRight.points}`;
+
+            if (gameObject.finishTime)
+                container.querySelector(".modal-game-history-card-date").textContent = gameObject.finishTime;
+            else
+            container.querySelector(".modal-game-history-card-date").textContent = gameObject.state;
+
+            //TODO: Create an event listner for the ongoing games
+
+            this.domManip.$id("modal-game-history-body").appendChild(container);
+        }
     },
 
     hooks: {
         beforeOpen () {
-            // This function prepares the modal
-            // On sucess returns true, on failure returns false
-            // Will be called by the ModalManager
+            try {
+                // Try to store userId as Number
+                this.userId = parseInt(this.domManip.$id("router-view").getAttribute("data-user-id"));
+            } catch {
+                console.error("friendListModal: Couldn't find the userId attribute in the view");
+                return false;
+            }
+            if (!this.userId) {
+                console.error("friendListModal: Couldn't find the userId attribute in the view");
+                return false;
+            }
+            call(`game/history/${this.userId}/`, 'GET').then(data => {
+                console.log("data", data);
+                this.data = data;
+
+                this.cleanUpGameList();
+                if (data.games.length)
+                    this.domManip.$id("modal-game-history-no-games").style.display = "none";
+
+                for (let element of data.games)
+                    this.createGameCard(element);
+            })
             return true;
         },
         afterClose () {
