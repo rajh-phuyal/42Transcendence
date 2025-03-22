b9eaab6d76baed10bcfa0188f8a46ce377adfa41
Alexander Stein <stein.alexander@posteo.de>
2025-03-22 18:50:00 +0100

    modal translation done

--- Frontend/js/modals/tournamentMain/configs.js
+++ Frontend/js/modals/tournamentMain/configs.js
@@ -9,18 +9,16 @@ export default {
 
     methods: {
         updateModalView() {
-            /*
-            This function shows / hides the buttons
+            /* This function shows / hides the buttons
             depending on if the client is already enrolled in a tournament
                 enrolled:       show history & lobby buttons
                 not enrolled:   show history,create & join buttons
-            This function also updates the title
-            */
+            This function also updates the title */
            console.log("updateModalView");
            console.log(this.tournamentId);
             if(this.tournamentId){
                 // Enrolled
-                this.domManip.$id("modal-tournament-main-title").textContent = translate("tournamentMain", "title-enrolled");
+                this.domManip.$id("modal-tournament-main-title").textContent = translate("tournamentMain", "titleSubscribed");
                 this.domManip.$id("modal-tournament-main-btn-create").style.display = "none";
                 this.domManip.$id("modal-tournament-main-btn-join").style.display = "none";
                 this.domManip.$id("modal-tournament-main-btn-history").style.display = "block";
@@ -29,7 +27,7 @@ export default {
                 this.domManip.$on(btnLobby, "click", this.callBackLobby);
             } else{
                 // Not enrolled
-                this.domManip.$id("modal-tournament-main-title").textContent = translate("tournamentMain", "title-normal");
+                this.domManip.$id("modal-tournament-main-title").textContent = translate("tournamentMain", "titleNormal");
                 this.domManip.$id("modal-tournament-main-btn-create").style.display = "block";
                 this.domManip.$id("modal-tournament-main-btn-join").style.display = "block";
                 this.domManip.$id("modal-tournament-main-btn-history").style.display = "block";
