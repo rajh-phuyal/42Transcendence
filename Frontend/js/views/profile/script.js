a294154ec467d74ea9e31ec9d57bcd00f77b8b20
FInacio97 <xico.inacio26@gmail.com>
2025-03-22 21:49:59 +0100

    profile view layouted

--- Frontend/js/views/profile/script.js
+++ Frontend/js/views/profile/script.js
@@ -4,11 +4,11 @@ import $store from '../../store/store.js';
 
 function populateUserInfo(res) {
     let username = $id("username");
-    username.textContent = translate("profile", "subject") + res.username;
+    username.innerHTML = `${translate("profile", "subject")}${res.username}`;
     const element = $id("avatar");
     element.src = window.origin + '/media/avatars/' + res.avatar;
     let birthName =$id("birth-name");
-    birthName.textContent = translate("profile", "birthName") + res.lastName + ", " + res.firstName;
+    birthName.innerHTML = `<b>${translate("profile", "birthName")}</b>${res.lastName}, ${res.firstName}`;
     let lastSeenText =$id("last-seen-text");
     let lastSeenImg =$id("last-seen-image");
 
@@ -16,20 +16,20 @@ function populateUserInfo(res) {
         lastSeenImg.src = "../../../../assets/icons_128x128/icon_online.png";
     else
         lastSeenImg.src = "../../../../assets/icons_128x128/icon_offline.png";
-    lastSeenText.textContent = translate("profile", "lastSeen") + res.lastLoginFormatted;
+    lastSeenText.innerHTML =`<b>${translate("profile", "lastSeen")}</b>${res.lastLoginFormatted}`;
     let language =$id("language");
-    language.textContent = translate("profile", "language") + res.language;
+    language.innerHTML = `<b>${translate("profile", "language")}</b>${res.language}`;
 }
 
 function populateStats(res) {
     let element = $id("stats-games");
-    element.textContent = translate("profile", "gamesWon") + res.stats.game.won + "/" + res.stats.game.played;
+    element.innerHTML = `${translate("profile", "gamesWon")}${res.stats.game.won}/${res.stats.game.played}`;
     element = $id("stats-tournament-first-place");
-    element.textContent = translate("profile", "firstPlace") + res.stats.tournament.firstPlace + "/" + res.stats.tournament.played;
+    element.innerHTML = `${translate("profile", "firstPlace")}${res.stats.tournament.firstPlace}/${res.stats.tournament.played}`;
     element = $id("stats-tournament-second-place");
-    element.textContent = translate("profile", "secondPlace") + res.stats.tournament.secondPlace + "/" + res.stats.tournament.played;
+    element.innerHTML = `${translate("profile", "secondPlace")}${res.stats.tournament.secondPlace}/${res.stats.tournament.played}`;
     element = $id("stats-tournament-third-place");
-    element.textContent = translate("profile", "thirdPlace") + res.stats.tournament.thirdPlace + "/" + res.stats.tournament.played;
+    element.innerHTML = `${translate("profile", "thirdPlace")}${res.stats.tournament.thirdPlace}/${res.stats.tournament.played}`;
 }
 
 function populateProgress(res, identity) {
